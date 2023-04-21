f62961d5217b5e4c157d7213e51cf003bfa3dec2
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-21 23:39:34 +0200

    chore: fix test label

--- packages/anviljs/src/utils/toFlagCase.test.ts
+++ packages/anviljs/src/utils/toFlagCase.test.ts
@@ -5,6 +5,6 @@ test.each([
   ["foo", "--foo"],
   ["fooBar", "--foo-bar"],
   ["fooBarBaz", "--foo-bar-baz"],
-])("toFlagCase(%s) === %s", (input, expected) => {
+])("toFlagCase(%s) -> %s", (input, expected) => {
   expect(toFlagCase(input)).toBe(expected);
 });
