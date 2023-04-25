02f176f42cc1d3647c1e5bcda88fd8c7ca1feb9e
Sebastian Lorenz <fubhy@fubhy.com>
2023-04-25 16:30:58 +0200

    chore: improve log printing in example

--- examples/example-vitest/tests/setup.ts
+++ examples/example-vitest/tests/setup.ts
@@ -11,11 +11,11 @@ afterAll(async () => {
   });
 });
 
-afterEach((context) => {
+afterEach(async (context) => {
   context.onTestFailed(async () => {
     // If a test fails, you can fetch and print the logs of your anvil instance.
     const logs = await fetchLogs("http://localhost:8545", pool);
     // Only print the 20 most recent log messages.
-    console.log(logs.slice(-20));
+    console.log(...logs.slice(-20));
   });
 });
